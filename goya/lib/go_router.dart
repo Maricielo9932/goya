6664333e6ae57507a1619cf84eece6bf6394ef41
Maricielo9932 <maricieloramoslopez078@gmail.com>
2024-11-21 09:09:36 -0500

    subo mi primer repositorio de flutter

---
+++ goya/lib/go_router.dart
@@ -0,0 +1,22 @@
+import 'home_screen.dart';
+import 'package:go_router/go_router.dart';
+import 'details_screen.dart';
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            return const DetailsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
---
+++ goya/lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/details'),
+          child: const Text('Go to the Details screen'),
+        ),
+      ),
+    );
+  }
+}
